b6a1650c180307062e31cb5c18592e1b7f4be20f
vacuname <836610245@qq.com>
2024-03-29 11:52:57 +0800

    绑定一点事件，准备完善Effect系统

--- Assets/Script/Game/Monster/Enemy/Enemy.cs
+++ Assets/Script/Game/Monster/Enemy/Enemy.cs
@@ -46,27 +46,38 @@ public class Enemy : Character
                 partDict.Add(part.gameObject, partList.key);
             }
         }
-        //读取部位的数据
+        //读取部位的数据，转换成部位的伤害倍率的字典
         foreach (var partSet in partSetting.setting)
             weakDict.Add(partSet.key, new WeaknessData(partSet.value));
         foreach (var partSet in partSetting.setting1)
             weakDict.Add(partSet.key, new WeaknessData(partSet.value.data));
 
+        //添加部位的生命值到ABS
         ABS.AttributeSetContainer.AddAttributeSet(new BodyAttr(partSetting));
+
+        //给ABS中部位的生命值变化添加事件
+        BodyAttr bodyAttr = ABS.AttrSet<BodyAttr>();
+        foreach (string name in bodyAttr.AttributeNames)
+        {
+            bodyAttr[name].onPostCurrentValueChange += OnBodyPartToughnessPost;
+        }
+            
     }
 
     protected override void OnHit(HitInfo hitInfo)
     {
-        //计算伤害、计算部位韧性、结算
+        //计算伤害
         string partName = partDict[hitInfo.target];
         WeaknessData weakData = weakDict[partName];
         float dmgMul = weakData.multiplyDict[hitInfo.type];
         float dmg = hitInfo.damage * dmgMul;
-        Debug.Log("dmg = " + dmg);
-        ABS.AttrSet<CharaAtrr>()["health"].SetCurValueRelative(dmg, Tags.Calc.Sub);
+        //Debug.Log("dmg = " + dmg);
+
+        //生命值和部位生命值都减少
+        ABS.AttrSet<CharaAtrr>().health.SetCurValueRelative(dmg, Tags.Calc.Sub);
         ABS.AttrSet<BodyAttr>()[partName].SetCurValueRelative(dmg, Tags.Calc.Sub);
-        Debug.Log(ABS.AttrSet<CharaAtrr>().health.Value.currentValue);
-        Debug.Log(partName+ " "+ABS.AttrSet<BodyAttr>()[partName].CurrentValue);
+        //Debug.Log(ABS.AttrSet<CharaAtrr>().health.Value.currentValue);
+        //Debug.Log(partName+ " "+ABS.AttrSet<BodyAttr>()[partName].CurrentValue);
     }
 
     protected override void OnDead()
@@ -82,5 +93,14 @@ public class Enemy : Character
     protected override void OnDeadStart()
     {
         
+    }
+
+    private void OnBodyPartToughnessPost(AttributeBase toughness,float old,float now)
+    {
+        if(old>0 && now<= 0)
+        {
+            toughness.RefreshCurValue();
+            //获得Buff 失衡
+        }
     }
 }
